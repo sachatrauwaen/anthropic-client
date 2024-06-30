691ffaafb735c670d3c49b382493283cff5b8568
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-06-30 12:00:37 -0500

    feat: add class to represent a callable function

---
+++ src/AnthropicClient/Models/AnthropicFunction.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+
+namespace AnthropicClient.Models;
+
+/// <summary>
+/// Represents a function that can be provided as a tool.
+/// </summary>
+public class AnthropicFunction
+{
+  /// <summary>
+  /// Gets the method of the function.
+  /// </summary>
+  public MethodInfo Method { get; }
+
+  /// <summary>
+  /// Gets the instance on which the method is invoked.
+  /// </summary>
+  public object? Instance { get; }
+
+  internal AnthropicFunction(MethodInfo method)
+  {
+    Method = method;
+  }
+
+  internal AnthropicFunction(MethodInfo method, object? instance)
+  {
+    Method = method;
+    Instance = instance;
+  }
+}
