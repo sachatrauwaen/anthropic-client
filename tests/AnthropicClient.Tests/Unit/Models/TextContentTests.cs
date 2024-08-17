025379423527677d9fab77ca92a5ca4004ea1571
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-08-17 17:53:20 -0500

    tests: add tests for overloaded constructor

--- tests/AnthropicClient.Tests/Unit/Models/TextContentTests.cs
+++ tests/AnthropicClient.Tests/Unit/Models/TextContentTests.cs
@@ -3,6 +3,11 @@ namespace AnthropicClient.Tests.Unit.Models;
 public class TextContentTests : SerializationTest
 {
   private readonly string _testJson = @"{ ""text"": ""text"", ""type"": ""text"" }";
+  private readonly string _testJsonWithCacheControl = @"{ 
+    ""text"": ""text"", 
+    ""cache_control"": { ""type"": ""ephemeral"" }, 
+    ""type"": ""text"" 
+  }";
 
   [Fact]
   public void Constructor_WhenCalled_ItShouldInitializeText()
@@ -15,9 +20,31 @@ public class TextContentTests : SerializationTest
   }
 
   [Fact]
+  public void Constructor_WhenCalledWithCacheControl_ItShouldInitializeProperties()
+  {
+    var expectedText = "text";
+    var cacheControl = new EphemeralCacheControl();
+
+    var result = new TextContent(expectedText, cacheControl);
+
+    result.Text.Should().Be(expectedText);
+    result.CacheControl.Should().BeSameAs(cacheControl);
+  }
+
+  [Fact]
   public void Constructor_WhenCalledAndTextIsNull_ItShouldThrowArgumentNullException()
   {
     var action = () => new TextContent(null!);
+
+    action.Should().Throw<ArgumentNullException>();
+  }
+
+  [Fact]
+  public void Constructor_WhenCalledWithCacheControlAndTextIsNull_ItShouldThrowArgumentNullException()
+  {
+    var cacheControl = new EphemeralCacheControl();
+
+    var action = () => new TextContent(null!, cacheControl);
 
     action.Should().Throw<ArgumentNullException>();
   }
@@ -30,6 +57,16 @@ public class TextContentTests : SerializationTest
     var actual = Serialize(content);
 
     JsonAssert.Equal(_testJson, actual);
+  }
+
+  [Fact]
+  public void JsonSerialization_WhenSerializedWithCacheControl_ItShouldHaveExpectedShape()
+  {
+    var content = new TextContent("text", new EphemeralCacheControl());
+
+    var actual = Serialize(content);
+
+    JsonAssert.Equal(_testJsonWithCacheControl, actual);
   }
 
   [Fact]
