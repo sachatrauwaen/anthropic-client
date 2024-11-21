c4d4d8a15bb690ae2b766a8f797b9e49f6965e60
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-11-21 13:42:42 -0600

    feat: add content type

--- tests/AnthropicClient.Tests/Unit/Models/ContentTypeTests.cs
+++ tests/AnthropicClient.Tests/Unit/Models/ContentTypeTests.cs
@@ -5,40 +5,30 @@ public class ContentTypeTests
   [Fact]
   public void Text_WhenCalled_ItShouldReturnText()
   {
-    var expected = "text";
-
-    var actual = ContentType.Text;
-
-    actual.Should().Be(expected);
+    ContentType.Text.Should().Be("text");
   }
 
   [Fact]
   public void Image_WhenCalled_ItShouldReturnImage()
   {
-    var expected = "image";
-
-    var actual = ContentType.Image;
-
-    actual.Should().Be(expected);
+    ContentType.Image.Should().Be("image");
   }
 
   [Fact]
   public void ToolUse_WhenCalled_ItShouldReturnToolUse()
   {
-    var expected = "tool_use";
-
-    var actual = ContentType.ToolUse;
-
-    actual.Should().Be(expected);
+    ContentType.ToolUse.Should().Be("tool_use");
   }
 
   [Fact]
   public void ToolResult_WhenCalled_ItShouldReturnToolResult()
   {
-    var expected = "tool_result";
+    ContentType.ToolResult.Should().Be("tool_result");
+  }
 
-    var actual = ContentType.ToolResult;
-
-    actual.Should().Be(expected);
+  [Fact]
+  public void Document_WhenCalled_ItShouldReturnDocument()
+  {
+    ContentType.Document.Should().Be("document");
   }
 }
