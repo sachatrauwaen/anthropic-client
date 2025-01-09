f8e01bf4878e07f823d524408ed59a2232960a7b
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-01-08 22:46:17 -0600

    feat: add class for representing batch statuses

---
+++ src/AnthropicClient/Models/MessageBatchStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace AnthropicClient.Models;
+
+/// <summary>
+/// Represents the status of a message batch.
+/// </summary>
+public static class MessageBatchStatus
+{
+  /// <summary>
+  /// The status of a message batch that is being canceled.
+  /// </summary>
+  public const string Canceling = "canceling";
+  
+  /// <summary>
+  /// The status of a message batch that is in progress.
+  /// </summary>
+  public const string InProgress = "in_progress";
+  
+  /// <summary>
+  /// The status of a message batch that has ended.
+  /// </summary>
+  public const string Ended = "ended";
+}
